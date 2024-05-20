36aaadf7bcedb1f3cc7787ffeb102fc13a06fe92
Raiyan <tajkierhaque@gmail.com>
2024-05-20 20:33:01 +0600

    project setup done

---
+++ src/app/utils/pick.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-prototype-builtins */
+function pick<T, TKeys extends keyof T>(obj: T, keys: TKeys[]) {
+    const modifiedObj: T = {} as T;
+
+    keys.forEach((key) => {
+        if (obj.hasOwnProperty(key)) {
+            modifiedObj[key] = obj[key];
+        }
+    });
+    return modifiedObj;
+}
+
+export default pick;
